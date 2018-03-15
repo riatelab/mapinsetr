7e109ae623040f9408635f2eb126a2631477ea35
rCarto <timothee.giraud@cnrs.fr>
2018-03-15 16:59:03 +0100

    go with sf objects

--- R/create_mask.R
+++ R/create_mask.R
@@ -1,55 +1,94 @@
 #' @title Create a Mask
 #' @name create_mask
-#' @description Create a mask based on a bounding box, a Spatial*DataFrame or defined interactively.
-#' @param bb either a bounding box or a Spatial*DataFrame
-#' @param prj  a projection string
+#' @description Create a mask based on a bounding box, an sf POLYGON or 
+#' MULTIPOLYGON or an interactively defined rectancle.
+#' @param bb either a bounding box (bbox object), a numeric vector (xmin, ymin, 
+#' xmax, ymax) or an sf POLYGON or MULTIPOLYGON object. 
+#' @param prj a projection string
 #' @param interactive define the mask interactively 
 #' @param add add the mask to the current plot
-#' @return A SpatialPolygonsDataFrame
-#' @import sp
-#' @examples 
-#' if(require(cartography)){
-#'  data(nuts2006)
-#'  mybbox <- rbind(c(3998249, 4720519), c(2323018,2879522)) 
-#'  mymask <- create_mask(bb = mybbox, prj = proj4string(nuts0.spdf), add=FALSE)
-#'  plot(nuts0.spdf, col = "grey80")
-#'  plot(mymask, add=TRUE, border = "red", lwd = 3)
-#'  mymask2 <- create_mask(bb = nuts0.spdf[nuts0.spdf$id=="BG",], 
-#'                         prj = proj4string(nuts0.spdf))
-#'  plot(mymask2, add=TRUE, border = "green", lwd = 3)
-#' }
+#' @return An sf object
+#' @import sf
 #' @export
 create_mask <- function(bb, prj, interactive = FALSE, add = FALSE){
-  if(interactive){
-    x <- graphics::locator(n=2, type="n")
-    minx <- min(x$x)
-    maxx <- max(x$x)
-    miny <- min(x$y)
-    maxy <- max(x$y)
-  }else{
-    if(methods::is(bb, "Spatial")){
-      x <- bbox(bb)
-    }else{
-      x <- bb
-    }
-    minx <- x[1,1]
-    maxx <- x[1,2]
-    miny <- x[2,1]
-    maxy <- x[2,2]
-  }
-  wkt <- paste0("POLYGON((", 
-                minx, " ", miny, ",",
-                minx, " ", maxy, ",", 
-                maxx, " ", maxy, ",",
-                maxx, " ", miny, ",", 
-                minx, " ", miny,
-                "))")  
-  mask <- rgeos::readWKT(wkt)
-  if(add){sp::plot(mask, add=T)}
-  proj4string(mask) <- prj
-  mask <- SpatialPolygonsDataFrame(Sr = mask, 
-                                   data = data.frame(id = 1), 
-                                   match.ID = F)
+  err <- simpleError("bb should have a CRS or prj should be provided.")
   
+  # for interactive
+  if(interactive){
+    if(missing(prj)){
+      stop(err)
+    }
+    x <- graphics::locator(n=2, type="n")
+    bb <- st_bbox(c(xmin = min(x$x), ymin = min(x$y), 
+                    xmax = max(x$x), ymax = max(x$y)), 
+                  crs =  prj)  
+  }
+  
+  # for numeric vector
+  if(is.numeric(bb) & length(bb)==4){
+    if(missing(prj)){
+      stop(err)
+    }
+    bb <- st_bbox(c(xmin = bb[1], ymin=bb[2], xmax = bb[3], ymax = bb[4]), crs =  prj)
+  }
+  
+  # for sf object
+  if(max(class(bb) %in% c("sf", "sfc"))==1){
+    if(is.na(st_crs(bb)) & missing(prj)){
+      stop(err)
+    }
+    bb <- st_bbox(bb)
+  }
+  
+  # prj for bbox objetc
+  if(is.na(st_crs(bb)) & missing(prj)){
+    stop(err)
+  }
+  
+  # build the mask
+  mask <- st_sf(id = 1, geometry = st_as_sfc(bb), crs = st_crs(bb))
+  
+  # plot the mask
+  if(add){plot_sf(st_geometry(mask), add=T)}
+
   return(mask)
 }
+# 
+# 
+
+
+# library(sf)
+# library(sp)
+# 
+# polysf <- mtq[1, ]
+# mask <- create_mask(bb = polysf, add = F)
+# 
+# plot(mtq$geometry)
+# plot(mask, add=T)
+# 
+# bb <- bbsp
+# create_mask(bb, prj = st_crs(mtq), add=T)
+# bb <- bbsf
+# create_mask(bb, add=T)
+# bb <- polysf
+# create_mask(bb, add=T)
+# create_mask(interactive = TRUE, prj = proj4string(as(mtq, "Spatial")), add=T)
+# 
+# mtq2 <- mtq
+# st_crs(mtq2) <- NA
+# polysf <- mtq2[1, ]
+# bbsf <- st_bbox(polysf)
+# polysp <- as(polysf, "Spatial")
+# bbsp <- bbox(polysp)
+# 
+# 
+# bb <- polysp
+# create_mask(bb)
+# bb <- bbsp
+# create_mask(bb, prj = st_crs(mtq))
+# bb <- bbsf
+# create_mask(bb)
+# bb <- polysf
+# create_mask(bb)
+# create_mask(interactive = TRUE, prj = proj4string(as(mtq, "Spatial")))
+# 
--- R/move_and_resize.R
+++ R/move_and_resize.R
@@ -1,93 +1,48 @@
 #' @title Move and Resize a Spatial*DataFrame
 #' @name move_and_resize
-#' @description Move and Resize a Spatial*DataFrame
-#' @param spdf the Spatial*Dataframe to resize and move.
-#' @param mask  a SpatialPolygonsDataFrame used to select the area to move an resize. 
+#' @description Move and Resize an sf POLYON or MULTIPOLYGON object
+#' @param x an sf POLYON or MULTIPOLYGON object to resize and move.
+#' @param mask  an sf POLYON or MULTIPOLYGON object used to select the area to move an resize. 
 #' @param xy coordinates used for the move process, bottomleft corner of the inset. 
-#' @param prj projection of the inset. 
+#' @param prj outputted projection of the inset. 
 #' @param k factor used to resize. 
-#' @return A SpatialPolygonsDataFrame
-#' @import sp
-#' @import maptools
-#' @import rgeos
-#' @examples
-#' if(require(cartography)){
-#'   data(nuts2006)
-#'   mybbox <- rbind(c(4235059,4507390),c(2559828, 2784798)) 
-#'   mymask <- create_mask(bb = mybbox, prj = proj4string(nuts0.spdf), add=FALSE)
-#'   inset <- move_and_resize(spdf = nuts0.spdf, mask = mymask, 
-#'                            xy = c(5566624, 3599815), k = 5)
-#'   plot(nuts0.spdf)
-#'   plot(mymask, lwd = 2, border = "red", add = TRUE)
-#'   plot(inset, add = TRUE)
-#' }
+#' @return An sf object
+#' @import sf
 #' @export
-move_and_resize <- function(spdf, mask = NULL, xy, prj = proj4string(spdf), k = 1){
-  # A faire fonction pour transformer une bbox en spdf ?
-  # argument pos pour l'encrage du carton
-
+move_and_resize <- function(x, mask = NULL, xy, prj = st_crs(x), k = 1){
   # default mask
-  if (is.null(mask)){
-    mask <- gBuffer(spdf)
+  if (missing(mask)){
+    mask <- st_union(x)
   }
   
-  # proj tests
-  stopifnot(is.projected(spdf), is.projected(mask))
-  stopifnot(proj4string(mask)==proj4string(spdf))
-  
+  # input proj tests
+  stopifnot(!is.na(st_crs(mask)), !is.na(st_crs(x)))
+
   # union mask
-  if(length(mask) > 1 ){
-    mask <- rgeos::gBuffer(mask)
+  if(nrow(mask) > 1 ){
+    mask <- st_union(mask)
   }
 
-  # intersect spdf with mask
-  inter <- rgeos::gIntersection(mask, spdf, byid = T)
-  df <- data.frame(id = sapply(methods::slot(inter, "polygons"), 
-                               methods::slot, "ID"), stringsAsFactors = F)
-  df <- data.frame(do.call('rbind', (strsplit(as.character(df$id)," "))))
-  df <- data.frame(id = df$X2, stringsAsFactors = F)
-  row.names(df) <- df$id
-  df <- data.frame(spdf@data[match(row.names(df),  row.names(spdf) ), ])
-  colnames(df) <- colnames(spdf@data)
-  row.names(inter) <- row.names(df)
-  spdf <- SpatialPolygonsDataFrame(Sr = inter, data = df, match.ID = T)
+  # intersect mask and x
+  options(warn=-1)
+  x <- st_collection_extract(st_intersection( x, st_geometry(mask)), 
+                             type = c("POLYGON"))
+  options(warn=0)
   
-  # add mask to spdf
-  x <- spdf[1, ]
-  x@polygons <- mask@polygons
-  x@polygons[[1]]@ID <- "YOLO"
-  row.names(x@data) <- row.names(x)
+  # add mask to x
+  xm <- x[1, ]
+  st_geometry(xm) <- st_geometry(mask)
+  x <- rbind(xm,x)
   
-  # resize
-  spdf <- rbind(x, spdf)
-  spdf_bb <- bbox(spdf) 
-  spdf_w <- spdf_bb[1,2] - spdf_bb[1,1]
-  spdf_h <- spdf_bb[2,2] - spdf_bb[2,1]
-  spdf_sizemax <- which.max(c(spdf_w, spdf_h))
-  
-  if(spdf_sizemax==1){
-    scale <- spdf_w * k
-  }else{
-    scale <- spdf_h * k
-  }
-  
-  # special k=1 case
-  if(k==1){
-    spdf_resized <- spdf
-  }else{
-    spdf_resized <- elide(obj = spdf, scale = scale)
-  }
-  spdf_resized <- maptools::elide(obj = spdf, scale = scale)
-
-  # move
-  xy <- xy - bbox(spdf_resized)[,1]
-  spdf_moved <- elide(obj = spdf_resized,  shift=xy)
+  # resize & move
+  cntrd <- st_centroid(st_combine(x))
+  xg <- (st_geometry(x) - cntrd) * k + cntrd 
+  st_geometry(x) <- xg + xy - st_bbox(xg)[1:2]
   
   # get rid of mask
-  spdf_moved <- spdf_moved[-1, ]
-
-  # project
-  proj4string(spdf_moved) <- prj
+  x <- x[-1,]
+  x <- st_cast(x, "MULTIPOLYGON")
+  st_crs(x) <- prj
   
-  return(spdf_moved)
+  return(x)
 }
